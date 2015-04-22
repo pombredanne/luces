c2d0d486222d458ebfda795ef90d0a0bdd51124c
bdharrington7 <BDHarrington7@gmail.com>
2015-04-22 15:58:53 -0700

    LIA-39805 adds ability to parse doc fields to the proper types based on a mapping file

--- src/main/java/com/lithium/luces/Luces.java
+++ src/main/java/com/lithium/luces/Luces.java
@@ -1,5 +1,5 @@
 /*
- * Convert.java
+ * Luces.java
  * Created on Jan 13, 2015
  *
  * Copyright 2015 Lithium Technologies, Inc.
@@ -25,6 +25,7 @@ import org.apache.lucene.util.Version;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 
 /**
@@ -33,24 +34,92 @@ import com.google.gson.JsonElement;
  */
 public class Luces {
 
+	private String typeName;
+	private JsonObject mapping;
+
 	@SuppressWarnings("unused")
 	public Luces(Version version) {
 		if (!version.equals(Version.LUCENE_36)) {
 			throw new UnsupportedOperationException("This library does not support Lucene version " + version.name());
 		}
-		Document doc = new Document(); // discard after check
 	}
 
+	/**
+	 * Specify a mapping JSON object to be able to convert document fields to their proper types
+	 * @param typeName Name of the type. Set to null to reset the mapping file and only output strings for the field
+	 *                 values
+	 * @param mapping mapping JSON object.
+	 * @return this
+	 */
+	public Luces mapping(String typeName, JsonObject mapping) {
+		if (null == typeName || null == mapping) {
+			this.typeName = null;
+			this.mapping = null;
+		} else {
+			this.typeName = typeName;
+			this.mapping = mapping;
+		}
+		return this;
+	}
+
+	/**
+	 * Gets a string representation of the JSON object that the document has been converted to
+	 * @param doc the Lucene document to convert
+	 * @param setPrettyPrint pretty print the JSON string
+	 * @return the String version of the JSON version of a document
+	 */
 	public String documentToJSONStringified(Document doc, boolean setPrettyPrint) {
 		Gson gson = setPrettyPrint ? new GsonBuilder().setPrettyPrinting().create() : new Gson();
 		return gson.toJson(documentToJSON(doc));
 	}
 
+	/**
+	 * Convert the document to a JSON representation of a Lucene document for indexing into Elasticsearch
+	 * @param doc the Lucene document
+	 * @return the JSON representation of the document
+	 */
 	public JsonElement documentToJSON(Document doc) {
 		HashMap<String, Object> fields = new LinkedHashMap<>();
 		List<Fieldable> docFields = doc.getFields();
-		for (Fieldable field : docFields) {
-			fields.put(field.name(), field.stringValue());
+		if (null != mapping && null != typeName) {
+			JsonObject propertiesJson = mapping.getAsJsonObject(typeName).getAsJsonObject("properties");
+			for (Fieldable field : docFields) {
+				JsonObject fieldDef = propertiesJson.getAsJsonObject(field.name());
+				if (null == fieldDef) {
+					fields.put(field.name(), field.stringValue());
+					continue;
+				}
+				JsonElement fieldType = fieldDef.get("type");
+				String fieldValue = field.stringValue();
+				Object parsedValue;
+				switch (fieldType.getAsString()) {
+					case "byte":
+						// FALL THROUGH
+					case "short":
+						// FALL THROUGH
+					case "integer":
+						// FALL THROUGH
+					case "long":
+						parsedValue = Long.parseLong(fieldValue.trim());
+						break;
+					case "float":
+						// FALL THROUGH
+					case "double":
+						parsedValue = Double.parseDouble(fieldValue.trim());
+						break;
+					case "boolean":
+						parsedValue = Boolean.parseBoolean(fieldValue.trim());
+						break;
+					default: // leave as string
+						parsedValue = fieldValue;
+						break;
+				}
+				fields.put(field.name(), parsedValue);
+			}
+		} else {
+			for (Fieldable field : docFields) {
+				fields.put(field.name(), field.stringValue());
+			}
 		}
 		return new Gson().toJsonTree(fields);
 	}
--- src/test/java/com/lithium/luces/LucesTest.java
+++ src/test/java/com/lithium/luces/LucesTest.java
@@ -26,21 +26,58 @@ import org.apache.lucene.util.Version;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.google.gson.JsonObject;
+
 
 /**
  * @author Brian Harrington
  */
 public class LucesTest {
 
+	private String TYPE = "testType";
+	private String LOGIN = "login";
+	private String FIRST_NAME = "name_first";
+	private String EMAIL = "email";
+	private String RATING = "rating";
+	private String VIEWS = "views";
+	private String NEG_BYTE = "negByteField";
+	private String REGISTERED = "registered";
+
 	@Test
-	public void testConvertOneField() {
+	public void testConvertOneDocumentWithMapping() {
 		String valid = "{\n" +
 				"  \"login\": \"trogdor\",\n" +
 				"  \"name_first\": \"Joe\",\n" +
 				"  \"name_last\": \"Schmo\",\n" +
 				"  \"email\": \"homestar@runner.com\",\n" +
 				"  \"signup\": \"12/23/2014\",\n" +
-				"  \"gender\": \"male\"\n" +
+				"  \"gender\": \"male\",\n" +
+				"  \"rating\": 4.2453,\n" +
+				"  \"views\": 655351,\n" +
+				"  \"negByteField\": -12,\n" +
+				"  \"registered\": true\n" +
+				"}";
+		Document doc = createMockFlatUserDocument();
+		Luces luces = new Luces(Version.LUCENE_36);
+		luces.mapping(TYPE, createMapping());
+		String json = luces.documentToJSONStringified(doc, true);
+//		System.out.println(json);
+		Assert.assertEquals(valid, json);
+	}
+
+	@Test
+	public void testConvertOneDocumentWithoutMapping() {
+		String valid = "{\n" +
+				"  \"login\": \"trogdor\",\n" +
+				"  \"name_first\": \"Joe\",\n" +
+				"  \"name_last\": \"Schmo\",\n" +
+				"  \"email\": \"homestar@runner.com\",\n" +
+				"  \"signup\": \"12/23/2014\",\n" +
+				"  \"gender\": \"male\",\n" +
+				"  \"rating\": \"  4.2453 \",\n" +
+				"  \"views\": \"  655351\",\n" +
+				"  \"negByteField\": \" -12 \",\n" +
+				"  \"registered\": \" true \"\n" +
 				"}";
 		Document doc = createMockFlatUserDocument();
 		Luces luces = new Luces(Version.LUCENE_36);
@@ -70,12 +107,52 @@ public class LucesTest {
 
 		Document doc = new Document();
 
-		doc.add(new Field("login", login.toLowerCase(), Store.YES, Index.NOT_ANALYZED));
-		doc.add(new Field("name_first", name_first, Store.NO, Index.ANALYZED));
+		doc.add(new Field(LOGIN, login.toLowerCase(), Store.YES, Index.NOT_ANALYZED));
+		doc.add(new Field(FIRST_NAME, name_first, Store.NO, Index.ANALYZED));
 		doc.add(new Field("name_last", name_last, Store.NO, Index.ANALYZED));
 		doc.add(new Field("email", email.toLowerCase(), Store.NO, Index.ANALYZED));
 		doc.add(new Field("signup", sdf.format(reg_date.getTime()), Store.NO, Index.NOT_ANALYZED));
 		doc.add(new Field("gender", gender, Store.NO, Index.ANALYZED));
+		// testing numbers and accounting for whitespace
+		doc.add(new Field(RATING, "  4.2453 ", Store.NO, Index.ANALYZED));
+		doc.add(new Field(VIEWS, "  655351", Store.NO, Index.ANALYZED));
+		doc.add(new Field(NEG_BYTE, " -12 ", Store.NO, Index.ANALYZED));
+		doc.add(new Field(REGISTERED, " true ", Store.NO, Index.ANALYZED));
+
 		return doc;
+	}
+
+	private JsonObject createMapping() {
+		JsonObject mapping = new JsonObject();
+		JsonObject typeObject = new JsonObject();
+		JsonObject propertiesObject = new JsonObject();
+
+		JsonObject loginDef = new JsonObject();
+		loginDef.addProperty("type", "string");
+		propertiesObject.add(LOGIN, loginDef);
+
+		JsonObject nameDef = new JsonObject();
+		nameDef.addProperty("type", "string");
+		propertiesObject.add(FIRST_NAME, nameDef);
+
+		JsonObject ratingDef = new JsonObject();
+		ratingDef.addProperty("type", "float");
+		propertiesObject.add(RATING, ratingDef);
+
+		JsonObject viewsDef = new JsonObject();
+		viewsDef.addProperty("type", "integer");
+		propertiesObject.add(VIEWS, viewsDef);
+
+		JsonObject byteDef = new JsonObject();
+		byteDef.addProperty("type", "byte");
+		propertiesObject.add(NEG_BYTE, byteDef);
+
+		JsonObject boolDef = new JsonObject();
+		boolDef.addProperty("type", "boolean");
+		propertiesObject.add(REGISTERED, boolDef);
+
+		typeObject.add("properties", propertiesObject);
+		mapping.add(TYPE, typeObject);
+		return mapping;
 	}
 }
